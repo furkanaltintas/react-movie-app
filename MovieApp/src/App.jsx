98ae75003ba7d8237467cc5d24a1fb555a5e6194
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-05-18 23:58:00 +0300

    Router ve Link işlemleri yapıldı

--- MovieApp/src/App.jsx
+++ MovieApp/src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { Path } from './constants/path'
 import Navbar from './components/Navbar/Navbar'
+import MyList from './pages/MyList/MyList'
+import Home from './pages/Home/Home'
+
+import './App.css'
 
 function App() {
   return (
     <div className='app'>
       <Navbar />
+      <Routes>
+        <Route path={Path.HOME} element={<Home />} />
+        <Route path={Path.MY_LIST} element={<MyList />} />
+      </Routes>
     </div>
   )
 }
--- MovieApp/src/components/Navbar/Navbar.jsx
+++ MovieApp/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,34 @@
+import { Link } from 'react-router-dom';
 import { PiFilmReelFill } from 'react-icons/pi';
 import { TiHome } from 'react-icons/ti';
 import { FaHeart } from 'react-icons/fa';
 
-
 import './Navbar.css';
+
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="left">
-                <h1>MovieApp</h1>
+                <Link to='/'>
+                    <h1>MovieApp</h1>
+                </Link>
             </div>
             <div className="center">
                 <PiFilmReelFill />
             </div>
             <div className="right">
                 <ul>
-                    <li><TiHome /></li>
-                    <li><FaHeart /></li>
+                    <li>
+                        <Link to='/'>
+                            <TiHome />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/my-list'>
+                            <FaHeart />
+                        </Link>
+                    </li>
                 </ul>
             </div>
         </nav>
