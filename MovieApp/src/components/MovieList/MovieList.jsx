ccf9de2eb19d6d82c27a9745a7df1eccef49aa10
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-05-20 01:05:55 +0300

    Sayfa tasarımı değiştirildi

--- MovieApp/src/components/MovieList/MovieList.jsx
+++ MovieApp/src/components/MovieList/MovieList.jsx
@@ -1,13 +1,13 @@
 import { useContext, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getMovieList, getMovieListByGenre } from '../../redux/slices/movieListSlice'
+import { GenreContext } from '../../contexts/GenreContext'
 
 import MovieCard from '../MovieCard/MovieCard'
 import Loading from '../Loading/Loading'
 import Error from '../Error/Error'
 
 import './MovieList.css'
-import { GenreContext } from '../../contexts/GenreContext'
 
 const MovieList = () => {
     const { movieList, status, error } = useSelector((store) => store.movieList)
@@ -21,23 +21,28 @@ const MovieList = () => {
         } else {
             dispatch(getMovieListByGenre(selectedGenre.id))
         }
-
     }, [selectedGenre, dispatch])
 
     return (
         <div className='movie-list'>
-            <h1>{selectedGenre ? selectedGenre.name : 'Discover'}</h1>
-            <ul>
-                {
-                    status === 'fulfilled' ?
-                        movieList && movieList.map((movie) => (
+            <div className='list-wrapper'>
+                <h1>{selectedGenre ? selectedGenre.name : 'Discover'}</h1>
+                {status === 'fulfilled' && (
+                    <div className="movie-grid">
+                        {movieList && movieList.map((movie) => (
                             <MovieCard movie={movie} key={movie.id} />
-                        )) :
-                        status === 'pending' ?
-                            <Loading /> :
-                            <Error error={error} />
-                }
-            </ul>
+                        ))}
+                    </div>
+                )}
+
+                {status === 'pending' && (
+                    <div className="loading-center">
+                        <Loading />
+                    </div>
+                )}
+
+                {status === 'rejected' && <Error error={error} />}
+            </div>
         </div>
     )
 }
